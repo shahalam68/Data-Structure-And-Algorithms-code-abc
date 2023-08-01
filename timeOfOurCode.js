8e4a551127e474c0b740ff041c5459556fbaf53a
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-08-01 21:33:54 +0600

    first commit

---
+++ timeOfOurCode.js
@@ -0,0 +1,21 @@
+// function sumOfAll(n){
+//     let total = 0; 
+//     for(let i=0; i<=n; i++){
+//         total += i;
+//     }
+//     return total;
+// }
+
+
+// function sumOfAll(n){
+//     return n * (n+1)/2
+// }
+// console.log(sumOfAll(5));
+
+
+function min5Log(n){
+    for(let i = 0; i <= Math.max(5,n); i++
+    ){
+        console.log(i);
+    }
+}
