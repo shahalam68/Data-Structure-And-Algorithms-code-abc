67dcb6f649da1ad9fa6c908bfb0ddb8c688c906b
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-09-05 00:44:25 +0600

    sumZero

--- timeOfOurCode.js
+++ timeOfOurCode.js
@@ -1,21 +1,28 @@
-// function sumOfAll(n){
-//     let total = 0; 
-//     for(let i=0; i<=n; i++){
-//         total += i;
+// // function sumOfAll(n){
+// //     let total = 0; 
+// //     for(let i=0; i<=n; i++){
+// //         total += i;
+// //     }
+// //     return total;
+// // }
+
+
+// // function sumOfAll(n){
+// //     return n * (n+1)/2
+// // }
+// // console.log(sumOfAll(5));
+
+
+// function min5Log(n){
+//     for(let i = 0; i <= Math.max(5,n); i++
+//     ){
+//         console.log(i);
 //     }
-//     return total;
 // }
 
-
-// function sumOfAll(n){
-//     return n * (n+1)/2
+// function max5log(n){
+//     for(let i = 0; i <= Math.min(5,n); i++){
+//         console.log(i);
+//     }
 // }
-// console.log(sumOfAll(5));
-
-
-function min5Log(n){
-    for(let i = 0; i <= Math.max(5,n); i++
-    ){
-        console.log(i);
-    }
-}
+console.log("soykot");
---
+++ zeroSum.js
@@ -0,0 +1,14 @@
+// Checking SumZero problem
+
+// [-5,-4,-3,-2,-0,2,4,6,8] -> input
+function sumZero (arr) {
+    for(let i of arr){
+        for(let j = 0 ; j = arr.length; j++ )
+        if(i + arr[j] === 0){
+            return [num, arr[j]]
+        }
+    }
+}
+
+const result = sumZero([-5,-4,-3,-2,-0,2,4,6,8] )
+console.log(result);
